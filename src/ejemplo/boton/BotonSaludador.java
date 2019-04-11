f65f54152a5f22281d8ab2028097e7d791cfe43f
Jaime Feldman B <jaimefel@gmail.com>
2019-04-10 22:08:25 -0400

    Primer ejemplo del tutorial donde se muestra como crear una ventana en javafx y un boton que no hace nada por el momento. aqui solo explica lo de stage y una un StackPane como layout

---
+++ src/ejemplo/boton/BotonSaludador.java
@@ -0,0 +1,45 @@
+package ejemplo.boton;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+
+
+/**
+ * @author Jaiem Feldman B.  10-04-2019
+ *	
+ *	Clase donde se estudia JavaFx para interfaces graficas modernas.
+ *	Aqui se despiega una ventana con un titulo y un boton al medio de un layout que no hace nada.
+ *
+ */
+public class BotonSaludador extends Application{
+
+	Button botonSaludo;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		primaryStage.setTitle("Ventana Saludador");
+
+		botonSaludo = new Button("Saludador");
+		
+		//Se crea el layout y se agrega el boton.
+		StackPane layout = new StackPane();
+		layout.getChildren().add(botonSaludo);
+
+		Scene esena = new Scene(layout, 500, 200);
+		
+		primaryStage.setScene(esena);
+		primaryStage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+}
